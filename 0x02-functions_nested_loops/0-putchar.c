2f1462a905a97ab386e6833f256c27218c890743
nzrr <bonuzx1@gmail.com>
2023-09-14 03:18:42 +0000

    first again

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,17 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * main - print _putchar
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
+char *c = "_putchar";
+while (*c != '\0')
+{
+_putchar(*c);
+c++;
+}
 _putchar('\n');
- return (0);
+return (0);
 }
